5d6546ffdbca116049e6ab66547e4d0b1900099d
Elankumaran98 <siva98kumarane@gmail.com>
2023-01-31 10:37:12 +0530

    ui created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,57 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import user from "./assest/img/user.png";
 
 function App() {
+  const [name, setName] = useState("");
   return (
-    <div className="App">
-      Laxi
+    <div className="container vh-100 d-flex justify-content-center align-items-center">
+      <div className="card border-0" style={{ width: "25rem" }}>
+        <nav className="navbar navbar-dark bg-dark rounded">
+          <div className="container-fluid">
+            <form
+              className="d-flex w-100"
+              onSubmit={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <input
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                value={name}
+                className="form-control me-2 w-100"
+                type="search"
+                placeholder="Enter our Name ..."
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-light" type="submit">
+                Predict
+              </button>
+            </form>
+          </div>
+        </nav>
+        <img src={user} className="card-img-top w-50 mx-auto p-3" alt="user" />
+        <div className="card-body">
+          <h5 className="card-title">Your Name is: {name}</h5>
+          <ul className="list-group ">
+            <li className="list-group-item d-flex justify-content-between align-items-start">
+              <div className="ms-2 me-auto">
+                <div className="fw-bold">Subheading</div>
+                Cras justo odio
+              </div>
+              <span className="badge bg-primary rounded-pill">14</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-start">
+              <div className="ms-2 me-auto">
+                <div className="fw-bold">Subheading</div>
+                Cras justo odio
+              </div>
+              <span className="badge bg-primary rounded-pill">14</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
