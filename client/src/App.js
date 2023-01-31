23f2044549eb0951582c45aa655d9618ef2c5fb7
Elankumaran98 <siva98kumarane@gmail.com>
2023-01-31 12:08:10 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,34 @@
 import { useState } from "react";
+import axios from "axios";
 import "./App.css";
 import user from "./assest/img/user.png";
 
 function App() {
   const [name, setName] = useState("");
-  const search=(e)=>{
+  const [response, setResponse] = useState();
+  const search = (e) => {
     e.preventDefault();
     console.log(name);
-    
-  }
+    axios.get("https://api.nationalize.io/?name=" + name).then((res) => {
+      console.log(res.data);
+      setResponse(res.data.country);
+    });
+  };
   return (
     <div className="container vh-100 d-flex justify-content-center align-items-center">
       <div className="card border-0" style={{ width: "25rem" }}>
         <nav className="navbar navbar-dark bg-dark rounded">
           <div className="container-fluid">
-            <form
-              className="d-flex w-100"
-              onSubmit={search}
-            >
+            <form className="d-flex w-100" onSubmit={search}>
               <input
+              value={name}
                 onChange={(e) => {
                   setName(e.target.value);
+                  if(e.target.value===''){
+                    setResponse(null);
+                  }
                 }}
-                value={name}
+                
                 className="form-control me-2 w-100"
                 type="search"
                 placeholder="Enter our Name ..."
@@ -38,20 +44,19 @@ function App() {
         <div className="card-body">
           <h5 className="card-title">Your Name is: {name}</h5>
           <ul className="list-group ">
-            <li className="list-group-item d-flex justify-content-between align-items-start">
-              <div className="ms-2 me-auto">
-                <div className="fw-bold">Subheading</div>
-                Cras justo odio
-              </div>
-              <span className="badge bg-primary rounded-pill">14</span>
-            </li>
-            <li className="list-group-item d-flex justify-content-between align-items-start">
-              <div className="ms-2 me-auto">
-                <div className="fw-bold">Subheading</div>
-                Cras justo odio
-              </div>
-              <span className="badge bg-primary rounded-pill">14</span>
-            </li>
+            {(response.length === 0)?<h1>No Result</h1>:()}
+            {response.map(country => {
+              return (
+                <li className="list-group-item d-flex justify-content-between align-items-start">
+                  <div className="ms-2 me-auto">
+                    <div className="fw-bold">{country.country_id}</div>
+                  </div>
+                  <span className="badge bg-primary rounded-pill">
+                    {(country.probability*100).toFixed(2)}%
+                  </span>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
