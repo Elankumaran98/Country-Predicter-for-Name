1a069daa36a50449be26d7d1d2b685d6bc800128
Elankumaran98 <siva98kumarane@gmail.com>
2023-03-29 09:43:25 +0530

    Navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import Predicter from "./pages/Predicter";
 
 function App() {
-  return <div>
-    <Predicter/>
-  </div>;
+  return (
+    <div>
+      <Predicter />
+    </div>
+  );
 }
 
 export default App;
