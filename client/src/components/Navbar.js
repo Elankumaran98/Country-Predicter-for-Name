d5291d226f88b0494b37f0fbc389aa2dd5c08b59
Elankumaran98 <siva98kumarane@gmail.com>
2023-03-29 11:38:25 +0530

    updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Link,NavLink } from 'react-router-dom'
+import {FiMenu,FiX} from 'react-icons/fi'
 import "../App.css"
 
 function Navbar() {
   return (
     <nav className='navbar'>
         <NavLink to='/' className='nav-logo'>Mr.K</NavLink>  
+        <div className='nav-icons'>
+            <FiMenu/>
+            <FiX/>
+        </div>
         <ul className='nav-links'>
-            <li><Link to='/' className='nav-link'>Home</Link></li>
-            <li><Link to='/menu' className='nav-link'>Menu</Link></li>
-            <li><Link to='/predicter' className='nav-link'>Predicter</Link></li>
-            <li><Link to='/about' className='nav-link'>About</Link></li>
+            <li className='nav-item'><Link to='/menu' className='nav-link'>Menu</Link></li>
+            <li className='nav-item'><Link to='/' className='nav-link'>Home</Link></li>
+            <li className='nav-item'><Link to='/predicter' className='nav-link'>Predicter</Link></li>
+            <li className='nav-item'><Link to='/about' className='nav-link'>About</Link></li>
 
         </ul>
     </nav>
