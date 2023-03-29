6d910f248a5ed008c2b92476baca0f060211e110
Elankumaran98 <siva98kumarane@gmail.com>
2023-03-29 11:05:27 +0530

    pages are linked

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
 import Predicter from "./pages/Predicter";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./components/About";
 
 function App() {
   return (
-    <div>
-      <Predicter />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="" element={<Home/>}/>
+          <Route path="/menu" element={<Menu/>}/>
+          <Route path="/predicter" element={<Predicter />} />
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import { Link,NavLink } from 'react-router-dom'
 import "../App.css"
 
 function Navbar() {
   return (
     <nav className='navbar'>
-        Mr.K
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+        <NavLink to='/' className='nav-logo'>Mr.K</NavLink>  
+        <ul className='nav-links'>
+            <li><Link to='/' className='nav-link'>Home</Link></li>
+            <li><Link to='/menu' className='nav-link'>Menu</Link></li>
+            <li><Link to='/predicter' className='nav-link'>Predicter</Link></li>
+            <li><Link to='/about' className='nav-link'>About</Link></li>
+
         </ul>
     </nav>
   )
